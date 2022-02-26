286feed88d2fa9116f19ad811ec6f8cc1c10779b
amandafadilaerros <amandafadila1@gmail.com>
2022-02-26 20:34:39 +0700

    choice user

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,30 +7,27 @@
     <div class="wrapper-bottom">
       <div class="header2 dark-blue">Masuk sebagai</div>
       <div class="wrapper-choise-user">
-        <a href="" class="choice-user"></a>
-        <a href="" class="choice-user"></a>
+        <a href="" class="choice-user">
+          <img src="@/assets/images/siswa.svg" alt="" />
+          <div class="text header2 blue">SISWA</div>
+        </a>
+        <a href="" class="choice-user"
+          ><img src="@/assets/images/lapak.svg" alt="" />
+          <div class="text header2 blue">LAPAK</div>
+        </a>
       </div>
     </div>
   </div>
 
   <LoginModal />
 </template>
-<script>
-// @ is an alias to /src
-import LoginModal from "./auth/components/loginModal.vue";
-
-export default {
-  name: "Home",
-  components: {
-    LoginModal,
-  },
-};
-</script>
+<script></script>
 <style scoped>
 .container-home {
   display: flex;
   flex-direction: column;
-  margin-top: 100px;
+  margin-top: 90px;
+  margin-bottom: 10px;
 }
 
 .wrapper-image img {
@@ -38,18 +35,30 @@ export default {
 }
 
 .wrapper-bottom {
+  margin: 30px;
   margin-top: 20px;
 }
 
 .wrapper-choise-user {
   display: flex;
+  padding: 10px;
 }
 
 .choice-user {
   width: 132px;
   height: 142px;
   background-color: var(--tosca);
-  margin: 5px auto;
+  margin: 10px auto;
   border-radius: 30px;
+}
+
+.choice-user img {
+  width: 70%;
+  margin-top: 10px;
+}
+
+.choice-user .text {
+  padding-bottom: 10px;
+  margin-bottom: 10px;
 }
 </style>
