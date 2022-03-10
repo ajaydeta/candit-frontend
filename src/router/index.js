b7c54491c35f2e0c4c4e7f9aaddfcc8c36ff0ad4
amandafadilaerros <amandafadila1@gmail.com>
2022-03-10 22:01:01 +0700

    detail pesan + menu list

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 import SiswaRoutes from "@/router/siswaRoutes";
 import AuthRoutes from "@/router/authRoutes";
 import MenuList from "@/views/siswa/MenuList";
+import DetailOrder from "@/views/siswa/DetailOrder";
 import LapakRoutes from "@/router/lapakRoutes";
 
 const routes = [
-    ...AuthRoutes,
-    SiswaRoutes,
-    LapakRoutes,
+  ...AuthRoutes,
+  SiswaRoutes,
+  LapakRoutes,
   {
     path: "/MenuList",
     name: "Menulist",
     component: MenuList,
   },
-]
+  {
+    path: "/DetailOrder",
+    name: "DetailOrder",
+    component: DetailOrder,
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
--- src/views/siswa/DetailOrder.vue
+++ src/views/siswa/DetailOrder.vue
@@ -1,22 +1,14 @@
 <template>
   <Base>
     <template #header>
-      <div class="header-menu-container">
-        <div class="header-menu-wrapper">
-          <p class="header2 dark-blue">Warung Lalapan Bu Sum</p>
-          <p class="header4 dark-blue">2 Makanan dipilih</p>
-          <p class="titleButton2 orange">Lihat Detail</p>
-        </div>
-        <div class="header-image-wrapper">
-          <img src="@/assets/images/lapak/lapak.png" alt="avatar" />
+      <div class="header-home-container">
+        <div class="header-text-wrapper">
+          <p class="header2">Detail Pesanan</p>
         </div>
       </div>
     </template>
     <template #content>
       <div class="menu-content">
-        <div class="menu-header">
-          <p class="header3 dark-blue">Lalapan + Nasi</p>
-        </div>
         <div class="menu-box">
           <div class="box-title">
             <p class="header3 dark-blue">Lalapan Tempe + Nasi</p>
@@ -45,7 +37,7 @@
 import { ref } from "vue";
 import Base from "@/components/ui/Base";
 export default {
-  name: "MenuList",
+  name: "DetailOrder",
   components: {
     Base,
   },
@@ -85,17 +77,8 @@ export default {
   box-shadow: var(--drop-shadow);
   border-radius: 20px;
 }
-.header-menu-wrapper {
+.header-text-wrapper {
   text-align: left;
-}
-.header-image-wrapper img {
-  margin: 0%;
-  /* padding: 10px; */
-  float: right;
-  text-align: left;
-  width: 81px;
-  height: 77px;
-  /* width: 20%; */
 }
 .menu-content {
   text-align: left;
